b72991c0ee1ea973dcd9527bbb686a6c6ec29138
Daniel Beardsley <daniel@ifixit.com>
2014-09-03 00:23:03 -0700

    Exit cleanly on a broken pipe

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var ActivityStream = require('./activity-stream.js');
 var SimplerEventLogger = require('./simple-event-logger.js');
 
-(new ActivityStream())
+var activityStream =  new ActivityStream();
+
+activityStream
 .pipe(new SimplerEventLogger())
 .pipe(process.stdout);
+
+process.stdout.on('error', function(err) {
+   if (err.code == 'EPIPE') {
+      process.exit(1);
+   }
+});

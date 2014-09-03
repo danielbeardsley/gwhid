b6254a1c4e752ca36b5ec63b21b84a6438db06b2
Daniel Beardsley <daniel@ifixit.com>
2014-09-03 00:26:29 -0700

    SimpleEventLogger: add custom output for different types
    There are many event types, I've listed a few of the important ones.

--- simple-event-logger.js
+++ simple-event-logger.js
@@ -1,17 +1,71 @@
 var Transform = require('stream').Transform;
 var util = require('util');
+var _ = require('underscore');
 
-function SimpleEventLogger() {
+function EventsToText() {
    Transform.call(this);
    this._readableState.objectMode = false;
    this._writableState.objectMode = true;
 }
-util.inherits(SimpleEventLogger, Transform);
+util.inherits(EventsToText, Transform);
 
-SimpleEventLogger.prototype._transform =
+EventsToText.prototype._transform =
 function(event, encoding, cb) {
-   this.push(event.repo.name + ": " + event.type + "\n");
+
+   var payload = event.payload || {};
+   var l = null;
+
+   switch (event.type) {
+      case "PushEvent": 
+         var branch = _.last(payload.ref.split('/'));
+         l = ["Pushed", payload.size, "commits to",  branch];
+         break;
+
+      case "PullRequestEvent": 
+         var pull = payload.pull_request;
+         // console.dir(event);
+         var pullInfo = "pull \"" + pull.title + "\" at " + pull.html_url;
+
+         switch (payload.action) {
+            case 'opened':
+               l = ["Opened", pullInfo];
+               break;
+            case 'closed':
+               if (pull.merged) {
+                  l = ["Merged", pullInfo];
+               } else {
+                  l = ["Closed", pullInfo];
+               }
+               break;
+
+         }
+         break;
+
+      case "CommitCommentEvent": 
+         l = ['Commented on a commit at', payload.html_url];
+         break;
+
+      case "CreateEvent": 
+         if (payload.ref_type == 'branch') {
+            l = ['Created a branch', payload.ref];
+         }
+         break;
+
+      case "IssueCommentEvent": 
+         l = ['Commented on an issue:', payload.issue.title, 'at', payload.comment.html_url ];
+         break;
+
+      case "PullRequestReviewCommentEvent":
+         l = ['Commented on a pull diff:', payload.pull_request.title, 'at', payload.comment.html_url ];
+         break;
+   }
+
+   if (l) {
+      this.push(l.join(" ") + "\n");
+   } else {
+      this.push(event.repo.name + ": " + event.type + "\n");
+   }
    cb();
 };
 
-module.exports = SimpleEventLogger;
+module.exports = EventsToText;

ba00a993e9c41a3f67d149abe60818adedef41c9
Daniel Beardsley <daniel@ifixit.com>
2017-06-22 14:31:00 -0700

    gwhid: add --org option
    Now you can see the events for a whole organization.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var ActivityStream = require('./activity-stream.js');
 var SimplerEventLogger = require('./simple-event-logger.js');
 
-var activityStream =  new ActivityStream();
+var argv = require('minimist')(process.argv.slice(2));
+console.log(argv);
+var org = argv.org;
+delete argv.org;
+if (Object.keys(argv) > 1) {
+   console.error("Usage: gwhid [--org=GithubOrg]");
+   console.error(" --org=GithubOrg: Show the events for the given organization");
+   console.error("                  instead of just the current user");
+   process.exit(1);
+}
+var activityStream =  new ActivityStream(org);
 
 activityStream
 .pipe(new SimplerEventLogger())
